0b7d870d433af408b65e8af4fdbdffac1d6a2e13
David Kestering <davidkestering@gmail.com>
2025-08-01 19:39:09 -0300

    Projeto Maven

---
+++ meuprojetomaven/src/main/java/br/com/bestbidev/meuprojetomaven/MainClass.java
@@ -0,0 +1,12 @@
+package br.com.bestbidev.meuprojetomaven;
+
+import com.google.gson.Gson;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Produto produto = new Produto(12345,"Computador",1500.0,5);
+		
+		Gson gson = new Gson();
+		System.out.println(gson.toJson(produto));
+	}
+}
