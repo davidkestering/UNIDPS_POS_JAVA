651c76fa6958f229c932103895d485c6b39f473e
David Kestering <davidkestering@gmail.com>
2025-08-21 11:23:34 -0300

    Arquitetura Multicamadas e Documentacao com SpringDoc Swagger

--- helloworld/src/main/java/com/bestbidev/helloworld/controller/HelloController.java
+++ helloworld/src/main/java/com/bestbidev/helloworld/controller/HelloController.java
@@ -1,18 +1,22 @@
 package com.bestbidev.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bestbidev.helloworld.model.Produto;
+import com.bestbidev.helloworld.controller.service.IMessageService;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+	@Qualifier("v2")
+	private IMessageService service;
+	
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello World";
+		return service.sayCustomMessage("Hello World");
 	}
 	
 	/*@GetMapping("/produtos")
---
+++ helloworld/src/main/java/com/bestbidev/helloworld/controller/service/IMessageService.java
@@ -0,0 +1,5 @@
+package com.bestbidev.helloworld.controller.service;
+
+public interface IMessageService {
+	public String sayCustomMessage(String original);
+}
